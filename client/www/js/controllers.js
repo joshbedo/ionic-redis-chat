acca8588a3e4776c34fed4457b995c294dd72178
Josh Bedo <joshbedo@yahoo.com>
2015-05-04 00:59:26 -0400

    Adding support for CORS, Adding Auth to login

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,9 +1,9 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['services'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $state, socket, Auth) {
   // Form data for the login modal
   $scope.loginData = {};
-
+  debugger
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
@@ -22,15 +22,41 @@ angular.module('starter.controllers', [])
   };
 
   // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
+  $scope.doLogin = function(user) {
     console.log('Doing login', $scope.loginData);
+    var user = $scope.loginData;
+    console.log(user);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+    Auth.login(user.username, user.password).then(function(data) {
+      console.log('auth passed');
+      if (data.success) {
+        console.log('successful login');
+        // Simulate a login delay. Remove this and replace with your login
+        // code if using a login system
+        $timeout(function() {
+          $scope.closeLogin();
+        }, 1000);
+      } else {
+        alert('Username / Password is incorrect');
+      }
+    });
   };
+})
+
+.controller('LoginCtrl', function($scope, $state, Auth) {
+  $scope.user = { name: '', password: '' };
+
+  $scope.login = function login(user) {
+    Auth.login(user.name, user.password).then(function(data) {
+      console.log('auth passed');
+      if (data.success) {
+        console.log('auth was successful');
+        $state.go('app');
+      } else {
+        alert('Username / password not valid. Try again');
+      }
+    });
+  }
 })
 
 .controller('PlaylistsCtrl', function($scope) {
@@ -45,4 +71,73 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
+});
+
+// SERVICES
+var baseUrl = 'http://localhost:1337';
+
+angular.module('services', [])
+
+.factory('socket', function socket($rootScope) {
+  var socket = io(baseUrl);
+
+  return {
+    on: function(eventName, cb) {
+      socket.on(eventName, function() {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          cb.apply(socket, args);
+        });
+      });
+    },
+    emit: function(eventName, data, cb) {
+      socket.emit(eventName, data, function() {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (cb) cb.apply(socket, args);
+        });
+      });
+    }
+  };
+})
+
+
+.factory('Auth', function Auth($q, $http) {
+  var user = null;
+
+  var login = function login (name, password) {
+    var defer = $q.defer();
+
+    var url = baseUrl + '/login';
+    var postData = { name: name, password: password };
+
+    $http.post(url, postData).success(function(response) {
+      if (response.success && response.success === true) {
+        user = { name: response.name, id: response.id };
+        window.localStorage.setItem('user', JSON.stringify(user));
+        return defer.resolve(response);
+      } else {
+        return defer.resolve('No user found');
+      }
+    }).error(function(err) {
+      defer.reject(err);
+    });
+
+    return defer.promise;
+  }
+
+  var currentUser = function currentUser (user) {
+    return user;
+  }
+
+  var logout = function logout () {
+    user = null;
+    window.localStorage.removeItem('user');
+  }
+
+  return {
+    login: login,
+    logout: logout,
+    currentUser: currentUser
+  };
 });
